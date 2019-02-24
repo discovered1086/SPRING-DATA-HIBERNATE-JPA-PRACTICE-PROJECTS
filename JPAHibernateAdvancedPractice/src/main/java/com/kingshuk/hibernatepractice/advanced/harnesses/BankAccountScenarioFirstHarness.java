42cffe517a6283e385d369f698271abe2710cedd
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-02-24 00:44:06 -0600

    Fixing some issues with json file and adding code for execution

--- JPAHibernateAdvancedPractice/src/main/java/com/kingshuk/hibernatepractice/advanced/harnesses/BankAccountScenarioFirstHarness.java
+++ JPAHibernateAdvancedPractice/src/main/java/com/kingshuk/hibernatepractice/advanced/harnesses/BankAccountScenarioFirstHarness.java
@@ -1,22 +1,40 @@
 package com.kingshuk.hibernatepractice.advanced.harnesses;
 
+import java.io.File;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kingshuk.hibernatepractice.advanced.configuration.ConfigurationUtil;
+import com.kingshuk.hibernatepractice.advanced.model.BankAccount;
 
 public class BankAccountScenarioFirstHarness {
-
+	
+	
 	public static void main(String[] args) {
 
 		Transaction theTransaction = null;
 		try (SessionFactory sessionFactory = ConfigurationUtil.buildSessionFactory();
 				Session session = sessionFactory.openSession()) {
+			
+			//Load the object to memory from the json file
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			BankAccount bankAccount = objectMapper.readValue(
+					new File("jsonfiles/BankAccount.json"), BankAccount.class);
 
 			// Begin the transaction
 			theTransaction = session.beginTransaction();
-
+			
+			bankAccount.getAccountHolder().getAddressList().forEach(session::save);
+			
+			session.save(bankAccount.getAccountHolder());
+			
+			//Persist the bank account
+			session.save(bankAccount);
+			
 			// Commit the transaction
 			theTransaction.commit();
 		} catch (Exception e) {
